e06d1d4966863033972fee0d41f3548d730dab0b
mg27713 <68036487+mg27713@users.noreply.github.com>
2020-07-08 16:35:08 -0700

    Add the preprocessing file.

---
+++ shors-classical-part1.js
@@ -0,0 +1,12 @@
+// These are the parameters. We want to factor the number 15, and our premature guess is 11.
+const k = 11; // Our premature guess
+const N = 15; // The number to factor
+
+// How long our sequence wants to be. A higher number means a greater chance of success, but it takes longer to compute.
+const sampleSize = 6;
+
+var mods = []; // Makes a list.
+for (var i = 0; i < sampleSize; i++) // Repeat however many times our sample size was, in this case 6.
+    mods.push((k**i)%N); // Finds how much more k^i is than a multiple of N, and adds it to the mods list.
+
+document.body.innerHTML = mods.toString(); // Prints the list to the page, for us to see.
