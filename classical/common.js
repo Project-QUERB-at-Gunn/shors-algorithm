36e044b3e6dc93c41d8ddea11fab8ecd617a83a9
mg27713 <68036487+mg27713@users.noreply.github.com>
2020-07-11 18:09:01 -0700

    Add M to common.js

--- classical/common.js
+++ classical/common.js
@@ -1,3 +1,4 @@
 // These are the parameters. We want to factor the number 15, and our premature guess is 11.
 export const k = 11; // Our initial factor guess
 export const N = 15; // The number to factor
+export const M = 6; // The range of i values for which to compute f(i). A higher M value means a greater chance of accuracy, but it takes longer to compute.
