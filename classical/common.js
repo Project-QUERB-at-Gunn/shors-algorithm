eb89e47ac7b4e3cad51a0db7b5d21e3fab126943
mg27713 <68036487+mg27713@users.noreply.github.com>
2020-07-11 18:23:16 -0700

    Add some more explanation to common.js

--- classical/common.js
+++ classical/common.js
@@ -1,3 +1,9 @@
+// This file contains code that is used by both
+// preprocessing.js and postprocessing.js. They both
+// have an "import" statement at the top that tells
+// the computer to look here first, and load the
+// variables we define here.
+
 // These are the parameters. We want to factor the number 15, and our premature guess is 11.
 export const k = 11; // Our initial factor guess
 export const N = 15; // The number to factor
