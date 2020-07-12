6ca8086d4b6b4e1bd729961964007afd002140ee
mg27713 <68036487+mg27713@users.noreply.github.com>
2020-07-11 18:21:22 -0700

    Complete the code in postprocessing.js.

--- classical/postprocessing.js
+++ classical/postprocessing.js
@@ -5,15 +5,26 @@ import { N, k, M } from './common';
 // First we define a gcd(x,y) function to find the greatest
 // common divisor of two integers by using the classically
 // efficient Euclidean algorithm. 
-export var gcd = function(x, y) {
+function gcd(x, y) {
     if (y == 0) {
         return x;
     }
-    return gcd (y, x % y);
+    return gcd (y, x % y); // x % y = x mod y = the remainder from x/y
 };
 
-// Our period returned by the quantum computer is defined
-const p = 2;
+function multiGcd(values) {
+    var currentGcd = values[0]; // values[0] is the first value
+    
+    for (var h = 1; h < values.length; h++) // Repeat for values[1] through the last element.
+        currentGcd = gcd(currentGcd, values[h]); // Accumulate a GCD for the entire list.
+    
+    return currentGcd; // currentGcd is now the result, because it is a common GCD among all values in the list.
+}
+
+// The results returned by the quantum computer is defined
+const results = []; // TBD. Not yet found.
+const L = multiGcd(results); // Find the GCD of every element in the results list, and call that value L.
+const p = M/L; // p=M/L.
 
 // Define what factors are divisible by N = 15
 const divs = {
