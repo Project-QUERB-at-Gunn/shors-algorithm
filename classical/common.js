77a4e0536195321efe66264494bf7f202995a025
mg27713 <68036487+mg27713@users.noreply.github.com>
2020-07-11 18:07:46 -0700

    Create common.js

---
+++ classical/common.js
@@ -0,0 +1,3 @@
+// These are the parameters. We want to factor the number 15, and our premature guess is 11.
+export const k = 11; // Our initial factor guess
+export const N = 15; // The number to factor
