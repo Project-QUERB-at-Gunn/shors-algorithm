9fee2c02239838280dc0a0b9d58eca69cb20b952
mg27713 <68036487+mg27713@users.noreply.github.com>
2020-07-11 18:27:46 -0700

    Made everything more consistent.

--- classical/preprocessing.js
+++ classical/preprocessing.js
@@ -5,4 +5,4 @@ var mods = []; // Makes a list.
 for (var i = 0; i < M; i++) // Repeat however many times our sample size was, in this case 6.
     mods.push((k**i)%N); // Finds how much more k^i is than a multiple of N, and adds it to the mods list.
 
-document.body.innerHTML = mods.toString(); // Prints the list to the page, for us to see.
+console.log(mods.toString()); // Prints the list to the console.
